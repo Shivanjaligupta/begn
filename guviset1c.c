069427fe89cf53c1a19c22b9a79f1ff0c7ef6500
Shivanjali gupta <35606688+Shivanjaligupta@users.noreply.github.com>
2018-01-23 22:46:28 +0530

    Add files via upload

---
+++ guviset1c.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main()
+{
+	 char v;
+	 printf("enter a character");
+	 scanf("%c",&v);
+	 if(v=='a'||v=='e'||v=='o'||v=='i'||v=='u'||v=='A'||v=='E'||v=='I'||v=='O'||v=='U')
+	 {
+	 	printf("vowel");
+	 }
+	 else
+	 {
+	 	printf("consonant");
+	 }
+}
+
