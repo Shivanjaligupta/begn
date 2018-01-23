20e6400f2892b21d94b7af35b532f07b0c962677
Shivanjali gupta <35606688+Shivanjaligupta@users.noreply.github.com>
2018-01-23 23:57:26 +0530

    Add files via upload

--- guviset1e.c
+++ guviset1e.c
@@ -4,7 +4,7 @@ void main()
     int n1, n2, n3;
 
     printf("Enter three different numbers: ");
-    scanf("%d %d %d", &n1, &n2, &n3);
+    scanf("%d\t %d\t %d", &n1, &n2, &n3);
 
     if( n1>=n2 && n1>=n3 )
         printf("%d is the largest number.", n1);
